a37caa487aed8b12cc92c8f5d266f1d7ccb3a46a
Egii <regidesoftian@gmail.com>
2022-02-19 07:05:09 +0000

    ci: move llvm repo scripts
    Signed-off-by: Egii <regidesoftian@gmail.com>

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #build script as LLVM Build Script.
-git clone https://github.com/rubyzee/llvmTC $(pwd)/llvmTC -b master
+git clone https://github.com/alicedump/llvmTC $(pwd)/llvmTC -b x86
 cd $(pwd)/llvmTC
 bash build-tc.sh
