e6323c0aae5de9fb6dc83dc89cff46e0d0c93234
Egii <regidesoftian@gmail.com>
2022-02-24 18:17:38 +0700

    ci: change branch

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #build script as LLVM Build Script.
-git clone https://github.com/alicedump/llvmTC $(pwd)/llvmTC -b x86
+git clone https://github.com/alicedump/llvmTC $(pwd)/llvmTC -b main
 cd $(pwd)/llvmTC
 bash build-tc.sh
